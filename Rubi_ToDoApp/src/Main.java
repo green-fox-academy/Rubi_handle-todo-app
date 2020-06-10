97127ffe4dfa6e89a0cfb630772a09ea8cc97b19
Rubi <rurikovec@gmail.com>
2020-06-10 13:41:29 +0200

    add print usage

--- Rubi_ToDoApp/src/Main.java
+++ Rubi_ToDoApp/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	if (args.length<1){printUsage();}
     }
-}
+    public static void printUsage(){
+        System.out.println ("Command line ToDo application");
+        System.out.println ("=============================");
+        System.out.println ("Command line arguments:");
+        System.out.println ("                       -l List all the tasks");
+        System.out.println ("                       -a Adds new task");
+        System.out.println ("                       -r Removes task");
+        System.out.println ("                       -c Completes task");
+    }
+    }
