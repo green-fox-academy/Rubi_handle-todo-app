4b5db1b2893e468cdcbfae6f780baad1560cf9ff
Rubi <rurikovec@gmail.com>
2020-06-11 15:49:41 +0200

    add Task Class

---
+++ Rubi_ToDoApp/src/Task.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Task implements ISave {
+    private static Integer idCount = 0;
+    private Integer id;
+    private boolean isCompleted;
+    private String name;
+    private String description;
+
+
+    public Task(boolean isCompleted, String name, String description) {
+        this.id = idCount + 1;
+        this.isCompleted = isCompleted;
+        this.name = name;
+        this.description = description;
+        idCount++;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return (isCompleted?"[X]":"[ ]" )+" Task: " + name + " Description: " + description + "\n";
+    }
+
+    @Override
+    public void saveToFile() {
+
+    }
+    public static String  createNewTask() {
+        Scanner scanner = new Scanner ( System.in );
+        System.out.println ("give me name");
+        String name = scanner.nextLine ();
+        System.out.println ("give me description");
+        String description = scanner.nextLine();
+        Task input=new Task(false,name,description);
+        return input.toString ();
+}}
